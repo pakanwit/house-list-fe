a2e850e04947b495e1e69f82167621bb107a1d5b
pakanwit oo <pakanwitk@pakanwits-oo.local>
2022-05-10 00:18:41 +0700

    Add InputWithLabel componnent

---
+++ src/Component/Input/InputWithLabel.tsx
@@ -0,0 +1,35 @@
+import styled from 'styled-components'
+import OutlinedInput, { OutlinedInputProps } from '@mui/material/OutlinedInput'
+import Typography from '@mui/material/Typography'
+
+interface InputWithLabelProps extends OutlinedInputProps {
+  width?: number
+  label: string
+}
+const CustomOutlineInput = styled(OutlinedInput)<InputWithLabelProps>`
+  &.MuiOutlinedInput-root {
+    height: 53px;
+    border-radius: 4;
+    background-color: #fff;
+    border: 1px solid #8d9196;
+    &.Mui-focused {
+      border: 1px solid #0f6fde;
+    }
+  }
+  input {
+    width: ${(props) => `${props.width}px`};
+  }
+  fieldset {
+    border: 0;
+  }
+`
+export const InputWithLabel = (props: InputWithLabelProps) => {
+  return (
+    <div>
+      <Typography variant='body1' display='block' component='div'>
+        {props.label}
+      </Typography>
+      <CustomOutlineInput {...props} />
+    </div>
+  )
+}
